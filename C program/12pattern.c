ea4e3c4fd3f48f1dcb02daa0f7f0677615aa6f87
herish05 <156744384+herish05@users.noreply.github.com>
2024-03-31 23:03:19 +0530

    Update 12pattern.c

--- C program/12pattern.c
+++ C program/12pattern.c
@@ -4,16 +4,16 @@ int main()
     int n;
     printf("enter number of rows");
     scanf("%d",&n);
-    for(int i=1;i<=n;i++){
+    for(int i=2;i<=n;i++){
         for(int j=1;j<=n;j++){
             if(i+j<=n){
                 printf("%s","0");
             }
             else if(i+j-1==n){
-                printf("%s","1");
+                printf("%s","2");
             }
             else{
-                printf("%s","2");
+                printf("%s","1");
             }
         }
         printf("\n");
